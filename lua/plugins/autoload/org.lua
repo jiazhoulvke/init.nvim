124c037f888e57e6525e21778d361147cdb7b075
jiazhoulvke <jiazhoulvke@gmail.com>
2022-02-10 01:29:14 +0800

    org

--- lua/plugins/autoload/org.lua
+++ lua/plugins/autoload/org.lua
@@ -37,47 +37,6 @@ use {
     end
 }
 
-use {
-    "lukas-reineke/headlines.nvim",
-	requires = "nvim-orgmode/orgmode",
-    config = function()
-        require("headlines").setup {
-            markdown = {
-                source_pattern_start = "^```",
-                source_pattern_end = "^```$",
-                dash_pattern = "^---+$",
-                headline_pattern = "^#+",
-                headline_highlights = {"Headline"},
-                codeblock_highlight = "CodeBlock",
-                dash_highlight = "Dash"
-            },
-            rmd = {
-                source_pattern_start = "^```",
-                source_pattern_end = "^```$",
-                dash_pattern = "^---+$",
-                headline_pattern = "^#+",
-                headline_signs = {"Headline"},
-                codeblock_sign = "CodeBlock",
-                dash_highlight = "Dash"
-            },
-            vimwiki = {
-                source_pattern_start = "^{{{%a+",
-                source_pattern_end = "^}}}$",
-                dash_pattern = "^---+$",
-                headline_pattern = "^=+",
-                headline_highlights = {"Headline"},
-                codeblock_highlight = "CodeBlock",
-                dash_highlight = "Dash"
-            },
-            org = {
-                source_pattern_start = "#%+[bB][eE][gG][iI][nN]_[sS][rR][cC]",
-                source_pattern_end = "#%+[eE][nN][dD]_[sS][rR][cC]",
-                dash_pattern = "^-----+$",
-                headline_pattern = "^%*+",
-                headline_highlights = {"Headline"},
-                codeblock_highlight = "CodeBlock",
-                dash_highlight = "Dash"
-            }
-        }
-    end
-}
+vim.cmd([[
+au FileType org setlocal conceallevel=2 concealcursor=nvc
+]])
